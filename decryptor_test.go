189a1dc9adaeb8556fbef7c5c4feada539a438ed
Ben Burkert <ben@benburkert.com>
2014-12-05 08:45:55 -0800

    test decryptor roundtrip

--- decryptor_test.go
+++ decryptor_test.go
@@ -2,6 +2,8 @@ package lockbox
 
 import (
 	"bytes"
+	"crypto/rand"
+	"encoding/pem"
 	"testing"
 
 	"golang.org/x/crypto/curve25519"
@@ -19,6 +21,41 @@ func TestDecryptionWithZeros(t *testing.T) {
 
 	want := []byte("Kill all humans")
 	got, err := d.Decrypt(zeroData)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if !bytes.Equal(got, want) {
+		t.Errorf("got '%s', want '%s'", got, want)
+	}
+}
+
+func TestDecryptionRoundtrip(t *testing.T) {
+	ekey, dkey, err := GenerateKey(rand.Reader)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	eb, _ := pem.Decode(ekey)
+	db, _ := pem.Decode(dkey)
+
+	e, err := encryptorFromBlock(eb)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	d, err := decryptorFromBlock(db)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	want := []byte("Kill all humans")
+	data, err := e.Encrypt(want)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	got, err := d.Decrypt(data)
 	if err != nil {
 		t.Fatal(err)
 	}
