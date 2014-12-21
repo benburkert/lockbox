48ecc2862038993f0c8a418642b1f6f3e943ade7
Ben Burkert <ben@benburkert.com>
2014-12-20 16:40:17 -0800

    refactor benchmarks

--- bench_test.go
+++ bench_test.go
@@ -11,6 +11,15 @@ import (
 var (
 	enc *Encryptor
 	dec *Decryptor
+
+	chunks = map[string][]byte{
+		"128B":  mustRandomBytes(128),
+		"4KB":   mustRandomBytes(4096),
+		"64KB":  mustRandomBytes(65536),
+		"512KB": mustRandomBytes(524288),
+		"1MB":   mustRandomBytes(1048576),
+		"4MB":   mustRandomBytes(4194304),
+	}
 )
 
 func init() {
@@ -33,14 +42,19 @@ func init() {
 	}
 }
 
-func BenchmarkLockboxEncrypt128(b *testing.B)  { lbEncrypt(128, b) }
-func BenchmarkLockboxEncrypt1024(b *testing.B) { lbEncrypt(1024, b) }
-func BenchmarkLockboxEncrypt4096(b *testing.B) { lbEncrypt(4096, b) }
-func BenchmarkLockboxEncrypt64KB(b *testing.B) { lbEncrypt(65536, b) }
+func BenchmarkLockboxEncrypt128B(b *testing.B)  { lbEncrypt("128B", b) }
+func BenchmarkLockboxEncrypt4KB(b *testing.B)   { lbEncrypt("4KB", b) }
+func BenchmarkLockboxEncrypt64KB(b *testing.B)  { lbEncrypt("64KB", b) }
+func BenchmarkLockboxEncrypt512KB(b *testing.B) { lbEncrypt("512KB", b) }
+func BenchmarkLockboxEncrypt1MB(b *testing.B)   { lbEncrypt("1MB", b) }
+func BenchmarkLockboxEncrypt4MB(b *testing.B)   { lbEncrypt("4MB", b) }
 
-func lbEncrypt(s int, b *testing.B) {
-	data := make([]byte, s)
-	io.ReadAtLeast(rand.Reader, data, s)
+func lbEncrypt(key string, b *testing.B) {
+	data, ok := chunks[key]
+	if !ok {
+		b.Fatalf("missing chunk %s", key)
+	}
+	b.SetBytes(int64(len(data)))
 
 	b.ResetTimer()
 	for n := 0; n < b.N; n++ {
@@ -48,21 +62,34 @@ func lbEncrypt(s int, b *testing.B) {
 	}
 }
 
-func BenchmarkLockboxDencrypt128(b *testing.B)  { lbDecrypt(128, b) }
-func BenchmarkLockboxDencrypt1024(b *testing.B) { lbDecrypt(1024, b) }
-func BenchmarkLockboxDencrypt4096(b *testing.B) { lbDecrypt(4096, b) }
-func BenchmarkLockboxDencrypt64KB(b *testing.B) { lbDecrypt(65536, b) }
+func BenchmarkLockboxDecrypt128(b *testing.B)   { lbDecrypt("128B", b) }
+func BenchmarkLockboxDecrypt4096(b *testing.B)  { lbDecrypt("4KB", b) }
+func BenchmarkLockboxDecrypt64KB(b *testing.B)  { lbDecrypt("64KB", b) }
+func BenchmarkLockboxDecrypt512KB(b *testing.B) { lbDecrypt("512KB", b) }
+func BenchmarkLockboxDecrypt1MB(b *testing.B)   { lbDecrypt("1MB", b) }
+func BenchmarkLockboxDecrypt4MB(b *testing.B)   { lbDecrypt("4MB", b) }
 
-func lbDecrypt(s int, b *testing.B) {
-	buf := make([]byte, s)
-	io.ReadAtLeast(rand.Reader, buf, s)
-	data, err := enc.Encrypt(buf)
+func lbDecrypt(key string, b *testing.B) {
+	chunk, ok := chunks[key]
+	if !ok {
+		b.Fatalf("missing chunk %s", key)
+	}
+	data, err := enc.Encrypt(chunk)
 	if err != nil {
 		panic(err)
 	}
+	b.SetBytes(int64(len(data)))
 
 	b.ResetTimer()
 	for n := 0; n < b.N; n++ {
 		dec.Decrypt(data)
 	}
+}
+
+func mustRandomBytes(s int) []byte {
+	buf := make([]byte, s)
+	if _, err := io.ReadAtLeast(rand.Reader, buf, s); err != nil {
+		panic(err)
+	}
+	return buf
 }
